6cf93f9ae95e00fc14227dbde1f058bbef01e191
tomoyasuzuki <tomoya.s0121@icloud.com>
2021-02-12 02:21:02 +0900

    refactor: refactor ethernet header parser

--- main.c
+++ main.c
@@ -9,6 +9,12 @@
 #include <features.h>
 #include <stdint.h>
 
+#define ETHERNET_HEADER_SIZE 14
+#define MAX_PACKET_SIZE 1000
+#define TYPE_IPV4 0x0800
+#define TYPE_IPV6 0x86dd
+#define TYPE_ARP 0x0806
+
 struct ether_hdr {
     uint8_t dst_addr[6];
     uint8_t src_addr[6];
@@ -16,43 +22,46 @@ struct ether_hdr {
 };
 
 int main(int argc, char **argv[]) {
-    // open tap devide
-    int tapfd = open("/dev/net/tun", O_RDWR);
+    int tapfd;
     struct ifreq ifr;
+    void *buff;
+    int rsize;
+
+    if ((tapfd = open("/dev/net/tun", O_RDWR)) < 0) {
+        perror("error");
+    }
+
     memset(&ifr, 0, sizeof(ifr));
     ifr.ifr_ifru.ifru_flags = IFF_TAP | IFF_NO_PI;
     strncpy(ifr.ifr_name, "tap77", IFNAMSIZ);
+    
     if (ioctl(tapfd, TUNSETIFF, &ifr) < 0) {
         perror("error");
     }
 
-    void *buff = malloc(1000);
-    // read packets
+    buff = malloc(MAX_PACKET_SIZE);
+
     for (;;) {
-        int size = read(tapfd, buff, 1000);
-        struct ether_hdr *ethdr = malloc(14);
-        memcpy(ethdr, buff, 14);
+        rsize = read(tapfd, buff, MAX_PACKET_SIZE);
+
+        struct ether_hdr *ethdr = malloc(ETHERNET_HEADER_SIZE);
+        memcpy(ethdr, buff, ETHERNET_HEADER_SIZE);
+
         uint16_t first = ethdr->type << 8;
         uint16_t second = ethdr->type >> 8;
         first |= second;
-        // 00001000 00000110
-        printf("0x%04x\n", first);
+
         char *type;
-        if (first == 0x0800) {
+        if (first == TYPE_IPV4) {
             type = "IPv4";
-        } else if (first == 0x0806) {
+        } else if (first == TYPE_ARP) {
             type = "ARP";
+        } else if (first == TYPE_IPV6) {
+            type = "IPv6";
         } else {
             type = "Unknown";
         }
 
-        printf("Read %d bytes type=0x%04x(=%s)\n", size, first, type);
-
-        for (int i = 0; i < 14; i++) {
-            printf("%x ", *((uint8_t*)buff+i));
-        }
-        printf("\n");
+        printf("Read %d bytes type=0x%04x(=%s)\n", rsize, first, type);
     }
-    // 10.0.0.1/24 
-    // parse packet and check protocol number and from address;
 }
